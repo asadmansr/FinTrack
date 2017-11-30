dd1322291c69859efb6500b42aca0d763dd125e4
asadmansr <asadmansr@gmail.com>
2017-11-29 19:40:24 -0500

    Added chart library and logos for toolbar

--- app/src/main/java/com/asadmansoor/fintrack/DashboardActivity.java
+++ app/src/main/java/com/asadmansoor/fintrack/DashboardActivity.java
@@ -15,12 +15,20 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
 import com.github.rubensousa.floatingtoolbar.FloatingToolbar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DashboardActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     EditText mSearchEditText;
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +38,7 @@ public class DashboardActivity extends AppCompatActivity
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab = (FloatingActionButton) findViewById(R.id.fab);
         final FloatingToolbar fabToolbar = (FloatingToolbar) findViewById(R.id.floatingToolbar);
         mSearchEditText = (EditText) findViewById(R.id.search_et);
 
@@ -43,6 +51,36 @@ public class DashboardActivity extends AppCompatActivity
         });
 
         fabToolbar.attachFab(fab);
+        fabToolbar.setClickListener(new FloatingToolbar.ItemClickListener() {
+            @Override
+            public void onItemClick(MenuItem item) {
+                int id = item.getItemId();
+
+                if (id == R.id.toolbar_bitcoin){
+                    fab.setImageResource(R.drawable.ic_account_balance_wallet_white_24dp);
+
+
+                } else if (id == R.id.toolbar_stock){
+                    fab.setImageResource(R.drawable.ic_account_balance_white_24dp);
+
+
+                } else if (id == R.id.toolbar_gold){
+                    fab.setImageResource(R.drawable.ic_redeem_white_24dp);
+
+
+                } else if (id == R.id.toolbar_currency){
+                    fab.setImageResource(R.drawable.ic_attach_money_white_24dp);
+
+
+                }
+            }
+
+            @Override
+            public void onItemLongClick(MenuItem item) {
+
+            }
+        });
+
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,6 +97,41 @@ public class DashboardActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+
+        LineChart chart = (LineChart) findViewById(R.id.chart);
+
+        List<Entry> entries = new ArrayList<Entry>();
+        entries.add(new Entry(1,2));
+        entries.add(new Entry(2,4));
+        entries.add(new Entry(3,5));
+        entries.add(new Entry(4,6));
+        entries.add(new Entry(5,8));
+
+        List<Entry> entriesB = new ArrayList<Entry>();
+        entriesB.add(new Entry(1,3));
+        entriesB.add(new Entry(2,3));
+        entriesB.add(new Entry(3,5));
+        entriesB.add(new Entry(4,7));
+        entriesB.add(new Entry(5,9));
+
+
+        LineDataSet dataSet = new LineDataSet(entries, "Base Currency"); // add entries to dataset
+        dataSet.setColor(R.color.colorPrimary);
+
+        LineDataSet dataSetB = new LineDataSet(entriesB, "Counter Currency");
+        //dataSetB.setColor(R.color.colorAccent);
+
+        //dataSet.setColors(new int[]{R.color.colorPrimary,R.color.colorAccent});
+
+        LineData lineData = new LineData(dataSet);
+
+        //dataSetB.setColor(R.color.colorAccent);
+        lineData.addDataSet(dataSetB);
+
+        chart.setData(lineData);
+        chart.invalidate(); // refresh
+
     }
 
     @Override
